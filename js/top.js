a6ad3aebc5dbc31cc097c196ebf255fae8ad3bcb
riekiuchi0821 <rie.kiuchi.studio@gmail.com>
2026-06-29 11:33:57 +0900

    Contactフォームのバリデーションを追加

--- js/top.js
+++ js/top.js
@@ -1,30 +1,41 @@
-const sendButton = document.getElementById("send-button");
-sendButton.addEventListener("click", validation);
+/* Contact Form の送信ボタン処理 */
 
+//Enterキーなどあらゆるsubmitイベントを監視するため、送信ボタンではなくFormで取得する
+const contactForm = document.querySelector(".contact-form");
+contactForm.addEventListener("submit", validation);
+
+//送信ボタン押下後のバリデーション
 function validation(event) {
+  //submitでのデフォルト処理（ブラウザがフォーム送信、ページ再読み込み）をキャンセルする
   event.preventDefault();
 
   const name = document.getElementById("yourname");
   const email = document.getElementById("email");
   const message = document.getElementById("message");
 
+  //バリデーション処理
+  //初期化
+  const errorMessages = document.querySelectorAll(".error-message");
+  errorMessages.forEach(function(message){
+    message.textContent = "";
+  });
+
   //名前チェック
-  const errorName = name.parentElement.querySelector('.error-message');
-  errorName.textContent('');
+  if(name.value.trim() ===""){
+    const errorText = name.nextElementSibling;
+    errorText.textContent = "お名前を入力してください";
+  }
 
-  if(name.value.trim() ===''){
-    console.log('test');
-    errorName.textContent('お名前を入力してください');
-  } 
-
-//   //メールアドレスチェック
-//   const emailPattern = /^[a-zA-Z0-9.]+@[a-zA-Z0-9.]+$/;
-//   if(!emailPattern.test(email))
-//     //errors.push('メールアドレスの入力形式が正しくありません');
-
-//   //メッセージチェック
-//   if(message.trim() === '')
-//     //errors.push('お問合せ内容を入力してください');
-
-
+  //メールアドレスチェック
+  const emailPattern = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if(!emailPattern.test(email.value)){
+    const errorText = email.nextElementSibling;
+    errorText.textContent = "メールアドレスの入力形式が正しくありません";
+  }
+ 
+  //メッセージチェック
+  if(message.value.trim() === ""){
+    const errorText = message.nextElementSibling;
+    errorText.textContent = "お問合せ内容を入力してください";
+  }
 }
